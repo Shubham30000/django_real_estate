2a9b2782ba6c726cc48d4a1cdada119ef5f1f417
Shubham30000 <shubhamsinghnda2001@gmail.com>
2025-06-05 09:34:16 +0530

    feat: register login update user profile

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -2,6 +2,16 @@ from .base import *
 import environ
 from pathlib import Path
 
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
+
 # Initialize environment variables
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()  # Load .env file
